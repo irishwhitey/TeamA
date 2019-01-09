d2336a75ce80b282c21280e5063dbd719031a1ca
Martin White <martin.white@codecomputerlove.com>
2019-01-09 18:15:55 +0000

    First passing #7

--- TeamA/Calculator.cs
+++ TeamA/Calculator.cs
@@ -10,6 +10,26 @@ namespace TeamA
             var toReturn = 0;
             try
             {
+                if (problem.Contains(" + ") && problem.Contains(" - "))
+                {
+                    var splitted = problem.Split(" ");
+                    Operation _currentOperation = new Plus();
+                    foreach (var item in splitted)
+                    {
+                        var fred = new Item(item);
+                        if (fred.IsOperation())
+                        {
+                            _currentOperation = Factory.FromSymbol(item);
+                        }
+                        else
+                        {
+                            toReturn = _currentOperation.Calculate(toReturn, int.Parse(item));
+                        }
+                    }
+
+                    return toReturn;
+                }
+
                 if (problem.Contains(" + "))
                 {
                     var splitted = problem.Split("+");
@@ -21,10 +41,10 @@ namespace TeamA
                     return toReturn;
                 }
 
-                if (problem.Contains("-"))
+                if (problem.Contains(" - "))
                 {
-                    var splitted = problem.Split("-");
-                    toReturn = int.Parse(splitted[0]) - int.Parse(splitted[1]);
+                    var splitted1 = problem.Split(" - ");
+                    toReturn = int.Parse(splitted1[0]) - int.Parse(splitted1[1]);
 
                     return toReturn;
                 }
@@ -44,6 +64,13 @@ namespace TeamA
 
                     return toReturn;
                 }
+
+                if (problem.Contains("^"))
+                {
+                    var splitted2 = problem.Split(" ^ ");
+                    return (int) Math.Pow(int.Parse(splitted2[0]), int.Parse(splitted2[1]));
+                    
+                }
             }
             catch (Exception e)
             {
@@ -51,6 +78,56 @@ namespace TeamA
             }
 
             return toReturn;
+        }
+    }
+
+    public interface Operation
+    {
+        int Calculate(int first, int second);
+    }
+
+    public class Plus :Operation
+    {
+        public int Calculate(int first, int second)
+        {
+            return first + second;
+        }
+    }
+
+    public class Subtract : Operation 
+    {
+        public int Calculate(int first, int second)
+        {
+            return first - second;
+        }
+    }
+
+    public class Factory
+    {
+        public static Operation FromSymbol(string item)
+        {
+            switch (item)
+            {
+                case "-": return new Subtract();
+                    break;
+                default:return new Plus();
+                
+            }
+        }
+    }
+
+    public class Item
+    {
+        private readonly string _item;
+
+        public Item(string item)
+        {
+            _item = item;
+        }
+
+        public bool IsOperation()
+        {
+            return _item == "+" || _item == "-" || _item == "*" || _item == "/";
         }
     }
 }
