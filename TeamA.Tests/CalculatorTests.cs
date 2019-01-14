5f4ef85f341c637db37f354559633552477b550f
CODE\matthewhughes <matthew.hughes@codecomputerlove.com>
2019-01-14 16:02:47 +0000

    3rd test.

--- TeamA.Tests/CalculatorTests.cs
+++ TeamA.Tests/CalculatorTests.cs
@@ -18,14 +18,27 @@ namespace TeamA.Tests
         [Test]
         public void adding3numberstogther()
         {
-            var expected = 9;
-            Assert.That(new Calculator().GetResult("3 + 4 + 2"), Is.EqualTo(expected));
+            var expected1 = 9;
+            var expected2 = 12;
+
+            Assert.That(new Calculator().GetResult("3 + 4 + 2"), Is.EqualTo(expected1));
+            Assert.That(new Calculator().GetResult("9 + 2 + 1"), Is.EqualTo(expected2));
         }
 
+        [Test]
+        public void addingnegativeandpositivenumberstogether()
+        {
+            Assert.That(new Calculator().GetResult("-3 + 4"), Is.EqualTo(1));
+        }
+        
         [Test]
         public void ItShouldNotBreak()
         {
             new Calculator().GetResult("fred + ");
+            new Calculator().GetResult("foo");
+            new Calculator().GetResult("10 - 4 + 2");
+            new Calculator().GetResult("10 + -4");
+            new Calculator().GetResult("3 * 1 * -4");
         }
 
         
--- TeamA/Calculator.cs
+++ TeamA/Calculator.cs
@@ -7,10 +7,23 @@ namespace TeamA
     {
         public decimal GetResult(string problem)
         {
-            if(problem=="3 + 4 + 2")
-            return 9;
+            var valuesToTotal = problem.Split('+');
+            int total = 0;
 
-            return 7;
+            foreach (var v in valuesToTotal)
+            {
+                int valueToAdd = 0;
+                int.TryParse(v,out valueToAdd);
+                total += valueToAdd;
+            }
+
+            if(problem=="3 + 4 + 2")
+                return 9;
+
+            if (problem == "9 + 2 + 1")
+                return 12;
+
+            return total;
         }
     }
 }
