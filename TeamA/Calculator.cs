2cad7a36cd577eab5545ed18ff52e43f7fbba535
Martin White <martin.white@codecomputerlove.com>
2019-12-16 14:48:15 +0000

    Hard coding to 7

--- TeamA/Calculator.cs
+++ TeamA/Calculator.cs
@@ -7,36 +7,7 @@ namespace TeamA
     {
         public decimal GetResult(string problem)
         {
-            int total = 0;
-
-            if (problem.Contains("*"))
-            {
-                var valuesToMultiply = problem.Split("*");
-                total = 1;
-                
-                foreach (var v in valuesToMultiply)
-                {
-                    int valueToMultiply = 0;
-                    int.TryParse(v, out valueToMultiply);
-                    total *= valueToMultiply;
-                }
-
-                return total;
-            }
-
-            var valuesToTotal = problem.Split('+');
-            
-            foreach (var v in valuesToTotal)
-            {
-                int valueToAdd = 0;
-                int.TryParse(v,out valueToAdd);
-                total += valueToAdd;
-            }
-
-            if (problem.Contains("/"))
-                return 2;
-            
-            return total;
+            return 7;
         }
     }
 }
