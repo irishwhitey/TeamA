c321c9e512af106368d70068e1fba1a4e7f778e4
Martin White <martin.white@codecomputerlove.com>
2019-12-17 11:23:18 +0000

    Rebaslining for use

--- TeamA.Tests/CalculatorTests.cs
+++ TeamA.Tests/CalculatorTests.cs
@@ -15,48 +15,5 @@ namespace TeamA.Tests
             Assert.That(new Calculator().GetResult("3 + 4"), Is.EqualTo(expected));
         }
 
-        [Test]
-        public void adding3numberstogther()
-        {
-            var expected1 = 9;
-            var expected2 = 12;
-
-            Assert.That(new Calculator().GetResult("3 + 4 + 2"), Is.EqualTo(expected1));
-            Assert.That(new Calculator().GetResult("9 + 2 + 1"), Is.EqualTo(expected2));
-        }
-
-        [Test]
-        public void addingnegativeandpositivenumberstogether()
-        {
-            Assert.That(new Calculator().GetResult("-3 + 4"), Is.EqualTo(1));
-        }
-
-        [Test]
-        public void candividenumbers()
-        {
-            Assert.That(new Calculator().GetResult("8 / 4"), Is.EqualTo(2));
-        }
-
-        [Test]
-        public void canmultiplynumbers()
-        {
-            Assert.That(new Calculator().GetResult("2 * 4"), Is.EqualTo(8));
-        }
-
-        [Test]
-        public void ItShouldNotBreak()
-        {
-            new Calculator().GetResult("fred + ");
-            new Calculator().GetResult("foo");
-            new Calculator().GetResult("10 - 4 + 2");
-            new Calculator().GetResult("10 + -4");
-            new Calculator().GetResult("3 * 1 * -4");
-        }
-
-        
-        //public void ItShouldTheCorrectAnswert(string problem, int expectedValue)
-        //{
-        //    Assert.That(new Calculator().GetResult(problem), Is.EqualTo(expectedValue));
-        //}
     }
 }
