c7312e2aef1d71bf73558cb4132132fcc0c84131
Martin White <martin.white@codecomputerlove.com>
2020-10-28 14:41:12 +0000

    Reseting code base

--- TeamA.Tests/CalculatorTests.cs
+++ TeamA.Tests/CalculatorTests.cs
@@ -8,52 +8,6 @@ namespace TeamA.Tests
     [TestFixture]
     public class CalculatorTests
     {
-        [Test]
-        public void ItShouldReturnSomething()
-        {
-            var expected = 7;
-            Assert.That(new Calculator().GetResult("3 + 4"), Is.EqualTo(expected));
-        }
 
-        [Test]
-        public void ItShouldAddTwoNumbers()
-        {
-            var expected = 9;
-            Assert.That(new Calculator().GetResult("5 + 4"), Is.EqualTo((expected)));
-        }
-
-        [Test]
-        public void ItShouldAddThreeNumbers()
-        {
-            var expected = 6;
-            Assert.That(new Calculator().GetResult("3 + 2 + 1"), Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void ItShouldSubtractTwoNumbers()
-        {
-            var expected = 1;
-            Assert.That(new Calculator().GetResult("3 - 2"), Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void ItShouldSubtractThreeNumbers()
-        {
-            var expected = 0;
-            Assert.That(new Calculator().GetResult("3 - 2 - 1"), Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void ItShouldMultiplyTwoNumbers()
-        {
-            var expected = 6;
-            Assert.That(new Calculator().GetResult("3 * 2"), Is.EqualTo(expected));
-        }
-        [Test]
-        public void ItShouldDivideTwoNumbers()
-        {
-            var expected = 3;
-            Assert.That(new Calculator().GetResult("6 / 2"), Is.EqualTo(expected));
-        }
     }
 }
